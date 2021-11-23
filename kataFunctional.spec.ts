3227e45721e43ffe59df42961dda7457b19f08de
jcgouleau <jean-charles.gouleau@savoye.com>
2021-11-23 19:59:25 +0100

    <type>  first test create match

--- kataFunctional.spec.ts
+++ kataFunctional.spec.ts
@@ -1,24 +1,28 @@
-import { Comparable, max } from "./kataaddition";
+const createMatch = (
+  field: Field,
+  player1: ServerPlayer,
+  player2: ReceiverPlayer
+): Match => ({ field, player1, player2 });
 
-describe("math", () => {
-  it("should return 4 for max(4, 3)", () => {
-    const a: Comparable = { value: 4 };
-    const b: Comparable = { value: 3 };
-    expect(max(a, b)).toBe(a);
-  });
-  it("should return 4 for max(3, 4)", () => {
-    const a: Comparable = { value: 3 };
-    const b: Comparable = { value: 4 };
-    expect(max(a, b)).toBe(b);
-  });
-  it("should return 4 for max(4, 4)", () => {
-    const a: Comparable = { value: 4 };
-    const b: Comparable = { value: 4 };
-    expect(max(a, b)).toBe(a);
-  });
-  it("should return 5 for max(5, 5)", () => {
-    const a: Comparable = { value: 5 };
-    const b: Comparable = { value: 5 };
-    expect(max(b, a)).toBe(b);
+type ServerPlayer = {};
+type ReceiverPlayer = {};
+
+type Match = {
+  field: Field;
+  player1: ServerPlayer;
+  player2: ReceiverPlayer;
+};
+
+type Field = {};
+
+describe("match", () => {
+  it("should create a match with 2 players", () => {
+    const field: Field = {};
+    const player1: ServerPlayer = {};
+    const player2: ReceiverPlayer = {};
+
+    const match: Match = createMatch(field, player1, player2);
+
+    expect(match).toStrictEqual({ field, player1, player2 });
   });
 });
