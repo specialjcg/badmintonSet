83573a08575ae4851496e9cd677ffffe86f4e525
jcgouleau <jean-charles.gouleau@savoye.com>
2022-06-16 09:35:19 +0200

    iteration 25 remove unused

---
+++ testCombine.spec.ts
@@ -0,0 +1,40 @@
+let day = ['Monday' , 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday'];
+const result: any[] = [];
+const characters = ["a", "b", "c", "d"];
+
+const combinations = (arr: string[], min = 1, max: number) => {
+    const combination = (arr: any[], depth: number) => {
+        if (depth === 1) {
+            return arr;
+        } else {
+            const result:string[] = combination(arr, depth - 1).flatMap((val: string) =>
+
+                 arr.filter(elem=>!val.includes(elem)).map((char) =>val +', '+ char)
+
+
+
+            );
+
+
+            return arr.concat(result);
+        }
+    };
+
+    return combination(arr, max).filter((val: string[]) => val.length >= min);
+};
+
+
+
+
+
+describe('testcombine', () => {
+    it('should combine', () => {
+        const result = combinations(day, 1, 7);
+        console.log('jsonListBeauty', JSON.stringify(result, null, 2));
+
+
+
+
+
+    });
+});
