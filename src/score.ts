d0a957dd860f19b1635dd25092f9cc585e874413
Jean-Charles GOULEAU <jean-charles.gouleau@savoye.com>
2023-05-17 14:15:55 +0200

    Changes by marc-gavanier

--- src/score.ts
+++ src/score.ts
@@ -25,18 +25,22 @@ export const setMatchScore = ({players, tours}: Session<ToProcess>, winner: Winn
 
 const previous = (tours: Tour<Status>[]): Tour<Ready>[] => tours.slice(0, -1);
 
-const hasWinner = ([playerResult1, playerResult2]: MatchResult<Status>, winner: Winner) => playerResult1.nom === winner.nom || playerResult2.nom === winner.nom
+const hasWinner = ([playerResult1, playerResult2]: MatchResult<Status>, winner: Winner) =>
+    playerResult1.nom === winner.nom || playerResult2.nom === winner.nom
 
-const getPlayerScore = (player1: PlayerResult<ToProcess>, winner: Winner) => player1.nom === winner.nom ? winner.score : Lose;
+const getPlayerScore = (player1: PlayerResult<ToProcess>, winner: Winner) =>
+    player1.nom === winner.nom ? winner.score : Lose;
 
 const setWinner = ([player1, player2]: MatchResult<ToProcess>, winner: Winner): MatchResult<Ready> => [
     {nom: player1.nom, score: getPlayerScore(player1, winner)},
     {nom: player2.nom, score: getPlayerScore(player2, winner)}
 ]
 
-const toMatchesWithScoreFor = (winner: Winner) => (matchResult: MatchResult<Status>): MatchResult<Status> => hasWinner(matchResult, winner) ? setWinner(matchResult, winner) : matchResult;
+const toMatchesWithScoreFor = (winner: Winner) => (matchResult: MatchResult<Status>): MatchResult<Status> =>
+    hasWinner(matchResult, winner) ? setWinner(matchResult, winner) : matchResult;
 
-const setMatchesScoreForLast = (tours: Tour<Status>[], winner: Winner): Tour<Status> => (tours.at(-1) ?? []).map(toMatchesWithScoreFor(winner));
+const setMatchesScoreForLast = (tours: Tour<Status>[], winner: Winner): Tour<Status> =>
+    (tours.at(-1) ?? []).map(toMatchesWithScoreFor(winner));
 
 const scoreRecord: Record<MatchScore, 1 | 2 | 3> = {
     Lose: 3,
